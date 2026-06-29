54f5f751b52be1296eaeec46f1db90d4c0c040fe
skipp <skipp@SKIPPER>
2026-06-29 16:44:28 -0400

    Add project files.

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ WebApplication1/EndPoints/WeatherEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.EndPoints
+{
+    public class WeatherEndpoint
+    {
+    }
+}
